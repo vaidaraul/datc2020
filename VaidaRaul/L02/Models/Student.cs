c3cb1aa9f1bf07bd72223b623838661c28a161d5
vaidaraul <48157919+vaidaraul@users.noreply.github.com>
2020-10-04 18:33:13 +0300

    Add files via upload

---
+++ VaidaRaul/L02/Models/Student.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace studentsAPI
+{
+    public class Student 
+    {
+        public int Id{get;set;}
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+        public string Faculty{get;set;}
+        public int Year{get;set;}
+
+        public Student()
+        {
+            
+        }
+    }
+}
